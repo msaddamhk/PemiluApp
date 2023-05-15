cfe553b8866b6b4879cab0d631bf59f74536f3aa
msaddamhk <saddamhk567@gmail.com>
2023-05-16 05:01:45 +0700

    menambahkan_chart_dashboard_kecamatan

--- app/Http/Controllers/kecamatan/DashboardKoorKecamatanController.php
+++ app/Http/Controllers/kecamatan/DashboardKoorKecamatanController.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers\kecamatan;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dpt;
+use App\Models\KoorDesa;
+use App\Models\KoorKecamatan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardKoorKecamatanController extends Controller
 {
     public function index()
     {
-        return view('kecamatan.dashboard.index');
+        $kecamatan = KoorKecamatan::where('user_id', auth()->user()->id)->first();
+        $koorDesas = KoorDesa::where('koor_kecamatan_id', $kecamatan->id)->pluck('id');
+        $jumlahDpt = Dpt::whereIn('desa_id', $koorDesas)->count();
+
+        $desaDptCounts = Dpt::whereIn('desa_id', $koorDesas)
+            ->select('desa_id', DB::raw('count(*) as total_dpt'))
+            ->groupBy('desa_id')
+            ->orderBy('total_dpt', 'desc')
+            ->take(10)
+            ->get();
+
+        $labels = [];
+        $data = [];
+
+        foreach ($desaDptCounts as $desaDptCount) {
+            $desa = KoorDesa::find($desaDptCount->desa_id);
+            if ($desa) {
+                $labels[] = $desa->name;
+                $data[] = $desaDptCount->total_dpt;
+            }
+        }
+
+        $jumlahdptlaki = Dpt::whereIn('desa_id', $koorDesas)
+            ->where('gender', 'laki-laki')
+            ->count();
+
+        $jumlahdptperempuan = Dpt::whereIn('desa_id', $koorDesas)
+            ->where('gender', 'perempuan')
+            ->count();
+
+        return view('kecamatan.dashboard.index', compact('kecamatan', 'labels', 'data', 'jumlahDpt', 'jumlahdptlaki', 'jumlahdptperempuan'));
     }
 }
--- resources/views/kecamatan/dashboard/index.blade.php
+++ resources/views/kecamatan/dashboard/index.blade.php
@@ -3,7 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 @section('content')
     <section class="p-3">
-        <h1 class="fw-semibold">Dashboard</h1>
+        <h3 class="fw-semibold">Dashboard</h3>
         <hr />
 
         <div class="">
@@ -12,243 +12,134 @@
         </div>
 
         <div class="mt-4">
-            <div class="row">
-                <div class="col-md-4 mb-3">
-                    <div class="card p-4 card-dpt" style="border-color: rgba(255, 99, 132, 1)">
-                        <h6>Jumlah Kota</h6>
-                        <h3 class="fw-bold">5</h3>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-4">
-                    <div class="card p-4 card-dpt" style="border-color: rgba(54, 162, 235, 1)">
-                        <h6>Jumlah Kecamatan</h6>
-                        <h3 class="fw-bold">50</h3>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-4">
+            <div class="row justify-content-center">
+                <div class="col-md-6 mb-4">
                     <div class="card p-4 card-dpt" style="border-color: rgba(255, 206, 86, 1)">
                         <h6>Jumlah Desa</h6>
-                        <h3 class="fw-bold">150</h3>
+                        <h5 class="fw-bold">{{ $kecamatan->jumlahDesa() }} Desa</h5>
                     </div>
                 </div>
-                <div class="col-md-4 mb-4">
+                <div class="col-md-6 mb-4">
                     <div class="card p-4 card-dpt" style="border-color: rgba(75, 192, 192, 1)">
                         <h6>Jumlah User</h6>
-                        <h3 class="fw-bold">20</h3>
+                        <h5 class="fw-bold">20</h5>
                     </div>
                 </div>
-                <div class="col-md-4 mb-5">
+                <div class="col-md-6 mb-5">
                     <div class="card p-4 card-dpt" style="border-color: rgba(153, 102, 255, 1)">
                         <h6>Jumlah DPT</h6>
-                        <h3 class="fw-bold">2.000</h3>
+                        <h5 class="fw-bold">{{ $jumlahDpt }} Orang</h5>
                     </div>
                 </div>
-                <div class="col-md-4 mb-5">
+                <div class="col-md-6 mb-5">
                     <div class="card p-4 card-dpt" style="border-color: rgba(255, 159, 64, 1)">
                         <h6>Countdown</h6>
-                        <h3 class="fw-bold">100</h3>
+                        <h5 class="fw-bold">100</h5>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="container-fluid card-dpt p-3 p-lg-5 rounded-4 border-0">
-            <div class="row mt-5">
-                <div class="col-md-4 my-auto">
-                    <h5>Kecamatan</h5>
-                    <canvas id="myChartBar1"></canvas>
-                    <script>
-                        var data = {
-                            labels: ["Syiah Kuala", "Jaya Baru", "Meuraxa", "Banda Raya", "Baiturrahman", "Lueng Bata", "Kuta Alam"],
-                            datasets: [{
-                                label: "My First Dataset",
-                                data: [65, 59, 80, 81, 56, 55, 40],
-                                backgroundColor: [
-                                    "rgba(255, 99, 132, 0.2)",
-                                    "rgba(54, 162, 235, 0.2)",
-                                    "rgba(255, 206, 86, 0.2)",
-                                    "rgba(75, 192, 192, 0.2)",
-                                    "rgba(153, 102, 255, 0.2)",
-                                    "rgba(255, 159, 64, 0.2)",
-                                    "rgba(255, 99, 132, 0.2)"
-                                ],
-                                borderColor: [
-                                    "rgba(255, 99, 132, 1)",
-                                    "rgba(54, 162, 235, 1)",
-                                    "rgba(255, 206, 86, 1)",
-                                    "rgba(75, 192, 192, 1)",
-                                    "rgba(153, 102, 255, 1)",
-                                    "rgba(255, 159, 64, 1)",
-                                    "rgba(255, 99, 132, 1)"
-                                ],
-                                borderWidth: 1
-                            }]
-                        };
+        <div class="container-fluid card-dpt rounded-4 border-0">
+            <div class="row mt-5 justify-content-center p-3 p-lg-5">
 
-                        var options = {
-                            scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true
-                                    }
-                                }]
-                            }
-                        };
-
-                        var myBarChart = new Chart(document.getElementById("myChartBar1"), {
-                            type: 'bar',
-                            data: data,
-                            options: options
-                        });
-                    </script>
-                </div>
-                <div class="col-md-4 my-auto">
+                <div class="col-md-12 mb-5">
                     <h5>Desa</h5>
-                    <canvas id="myChartBar2"></canvas>
-                    <script>
-                        var data = {
-                            labels: ["Jeulingke", "Lamgugob", "Punge Jurong", "Lampaseh Aceh", "Punge Ujong", "Pineung",
-                                "Kopelma Darussalam"
-                            ],
-                            datasets: [{
-                                label: "My First Dataset",
-                                data: [65, 59, 80, 81, 56, 55, 40],
-                                backgroundColor: [
-                                    "rgba(255, 99, 132, 0.2)",
-                                    "rgba(54, 162, 235, 0.2)",
-                                    "rgba(255, 206, 86, 0.2)",
-                                    "rgba(75, 192, 192, 0.2)",
-                                    "rgba(153, 102, 255, 0.2)",
-                                    "rgba(255, 159, 64, 0.2)",
-                                    "rgba(255, 99, 132, 0.2)"
-                                ],
-                                borderColor: [
-                                    "rgba(255, 99, 132, 1)",
-                                    "rgba(54, 162, 235, 1)",
-                                    "rgba(255, 206, 86, 1)",
-                                    "rgba(75, 192, 192, 1)",
-                                    "rgba(153, 102, 255, 1)",
-                                    "rgba(255, 159, 64, 1)",
-                                    "rgba(255, 99, 132, 1)"
-                                ],
-                                borderWidth: 1
-                            }]
-                        };
-
-                        var options = {
-                            scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true
-                                    }
-                                }]
-                            }
-                        };
-
-                        var myBarChart = new Chart(document.getElementById("myChartBar2"), {
-                            type: 'bar',
-                            data: data,
-                            options: options
-                        });
-                    </script>
-                </div>
-
-
-                <div class="col-md-4">
-                    <canvas id="myChart"></canvas>
-                    <script>
-                        var data = {
-                            labels: ["Perempuan", "Laki-Laki"],
-                            datasets: [{
-                                data: [300, 350],
-                                backgroundColor: ["#FF6384", "#36A2EB"],
-                                hoverBackgroundColor: ["#FF6384", "#36A2EB"]
-                            }]
-                        };
-                        var options = {
-                            responsive: true
-                        };
-                        var myPieChart = new Chart(document.getElementById("myChart"), {
-                            type: 'pie',
-                            data: data,
-                            options: options
-                        });
-                    </script>
-                </div>
-
-
-                <div class="col-md-8 mt-5">
-                    <h5>Kota</h5>
                     <canvas id="myChartBar3"></canvas>
-                    <script>
-                        var data = {
-                            labels: ["Kota Banda Aceh", "Kabupaten Aceh Besar", "Kabupaten Aceh Jaya", "Kota Sabang",
-                                "Kota Lhokseumawe", "kabupaten Aceh Barat",
-                                "Kabupaten Pidie"
-                            ],
+                </div>
+
+                <div class="col-md-6 mb-3 mb-lg-0">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Jumlah DPT</h5>
+                            <canvas id="myChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Usia</h5>
+                            <canvas id="myChart2"></canvas>
+                        </div>
+                    </div>
+                </div>
+
+                <script>
+                    var labels = {!! json_encode($labels) !!};
+                    var data = {!! json_encode($data) !!};
+
+                    var ctx = document.getElementById('myChartBar3').getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: labels,
                             datasets: [{
-                                label: "My First Dataset",
-                                data: [65, 59, 80, 81, 56, 55, 40],
+                                label: 'Total DPT',
+                                data: data,
                                 backgroundColor: [
-                                    "rgba(255, 99, 132, 0.2)",
-                                    "rgba(54, 162, 235, 0.2)",
-                                    "rgba(255, 206, 86, 0.2)",
-                                    "rgba(75, 192, 192, 0.2)",
-                                    "rgba(153, 102, 255, 0.2)",
-                                    "rgba(255, 159, 64, 0.2)",
-                                    "rgba(255, 99, 132, 0.2)"
+                                    'rgba(255, 99, 132, 0.8)',
+                                    'rgba(54, 162, 235, 0.8)',
+                                    'rgba(255, 206, 86, 0.8)',
+                                    'rgba(75, 192, 192, 0.8)',
+                                    'rgba(153, 102, 255, 0.8)',
+                                    'rgba(255, 159, 64, 0.8)',
+                                    'rgba(106, 187, 170, 0.8)',
+                                    'rgba(176, 102, 173, 0.8)',
+                                    'rgba(241, 130, 141, 0.8)',
+                                    'rgba(139, 125, 174, 0.8)'
                                 ],
-                                borderColor: [
-                                    "rgba(255, 99, 132, 1)",
-                                    "rgba(54, 162, 235, 1)",
-                                    "rgba(255, 206, 86, 1)",
-                                    "rgba(75, 192, 192, 1)",
-                                    "rgba(153, 102, 255, 1)",
-                                    "rgba(255, 159, 64, 1)",
-                                    "rgba(255, 99, 132, 1)"
-                                ],
+                                borderColor: 'rgba(255, 99, 132, 1)',
                                 borderWidth: 1
                             }]
-                        };
-
-                        var options = {
+                        },
+                        options: {
                             scales: {
-                                yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true
-                                    }
-                                }]
+                                y: {
+                                    beginAtZero: true
+                                }
                             }
-                        };
+                        }
+                    });
 
-                        var myBarChart = new Chart(document.getElementById("myChartBar3"), {
-                            type: 'bar',
-                            data: data,
-                            options: options
-                        });
-                    </script>
-                </div>
-                <div class="col-md-4 mt-5 my-auto">
-                    <canvas id="myChart2"></canvas>
-                    <script>
-                        var data = {
-                            labels: ["Remaja", "Dewasa", "Lansia"],
-                            datasets: [{
-                                data: [300, 50, 100],
-                                backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                                hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
-                            }]
-                        };
-                        var options = {
-                            responsive: true
-                        };
-                        var myPieChart = new Chart(document.getElementById("myChart2"), {
-                            type: 'pie',
-                            data: data,
-                            options: options
-                        });
-                    </script>
-                </div>
+
+                    var data = {
+                        labels: ["Perempuan", "Laki-Laki"],
+                        datasets: [{
+                            data: [{{ $jumlahdptperempuan }}, {{ $jumlahdptlaki }}],
+                            backgroundColor: ["#FF6384", "#36A2EB"],
+                            hoverBackgroundColor: ["#FF6384", "#36A2EB"]
+                        }]
+                    };
+                    var options = {
+                        responsive: true
+                    };
+                    var myPieChart = new Chart(document.getElementById("myChart"), {
+                        type: 'pie',
+                        data: data,
+                        options: options
+                    });
+
+
+                    var data = {
+                        labels: ["Remaja", "Dewasa", "Lansia"],
+                        datasets: [{
+                            data: [300, 50, 100],
+                            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+                            hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
+                        }]
+                    };
+                    var options = {
+                        responsive: true
+                    };
+                    var myPieChart = new Chart(document.getElementById("myChart2"), {
+                        type: 'pie',
+                        data: data,
+                        options: options
+                    });
+                </script>
+
             </div>
         </div>
 
