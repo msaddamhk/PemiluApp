4be91dec4a06742f9ce784caa84883734697798a
msaddamhk <saddamhk567@gmail.com>
2023-06-10 19:40:42 +0700

    rapikan_function_dashboard_general

--- app/Http/Controllers/General/DashboardGeneralController.php
+++ app/Http/Controllers/General/DashboardGeneralController.php
@@ -19,49 +19,20 @@ class DashboardGeneralController extends Controller
 
     public function index()
     {
-        $koorDesa = new KoorDesa();
-        $desaCount = $koorDesa->getCountForGeneral();
 
         $koorKota = new KoorKota();
         $kotaCount = $koorKota->getCountKotaForGeneral();
-
+        $cityData = $koorKota->getCityData();
         $getDataDiagram = $koorKota->getdataDiagram();
 
         $koorKecamatan = new KoorKecamatan();
         $kecamatanCount = $koorKecamatan->getCountKecamatanForGeneral();
 
+        $koorDesa = new KoorDesa();
+        $desaCount = $koorDesa->getCountForGeneral();
+
         $dpt = new Dpt();
         $dptCount = $dpt->getCountDptForGeneral();
-
-        $koorkotas = $koorKota->all();
-
-        $cityData = $koorkotas->map(function ($koorkota) {
-            $countDPT = DB::table('koor_desa')
-                ->join('dpt', 'koor_desa.id', '=', 'dpt.desa_id')
-                ->whereIn('koor_desa.koor_kecamatan_id', function ($query) use ($koorkota) {
-                    $query->select('id')
-                        ->from('koor_kecamatan')
-                        ->where('koor_kecamatan.koor_kota_id', $koorkota->id);
-                })
-                ->count();
-
-            $totalPopulation = KoorDesa::whereIn('koor_kecamatan_id', function ($query) use ($koorkota) {
-                $query->select('id')
-                    ->from('koor_kecamatan')
-                    ->where('koor_kecamatan.koor_kota_id', $koorkota->id);
-            })
-                ->sum('total_dpt');
-
-            $percentage = $totalPopulation > 0 ? number_format(($countDPT / $totalPopulation) * 100, 2) : 0;
-
-            return [
-                'kota' => $koorkota->name,
-                'slug' => $koorkota->slug,
-                'total_dpt' => $countDPT,
-                'total_penduduk' => $totalPopulation,
-                'persentase' => $percentage,
-            ];
-        });
 
         return view('general.dashboard.index', compact('cityData', 'desaCount', 'kotaCount', 'kecamatanCount', 'dptCount', 'getDataDiagram'));
     }
